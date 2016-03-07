67418805ae6712cb8249fc92498eba814c305d92
reverinu <reverinudog@yahoo.co.jp>
2016-03-08 03:07:37 +0900

    企画書のスクリプト部分の変更/各スクリプト・シーンの作成、整理

---
+++ Warwolf/Assets/Warwolf/Scripts/Game/GameNightSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameNightSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
